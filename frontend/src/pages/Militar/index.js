30d1308adc9ec13ef240b986fb74bdcc334b6e82
Mitchelson <mitchelsonps@gmail.com>
2020-03-12 10:12:21 -0400

    Troca de Tipo de Estilização

--- frontend/src/pages/Militar/index.js
+++ frontend/src/pages/Militar/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
-
-import { Container, Title, Box, Painel, Formulario, Registros } from './styles';
+import './styles';
 
 function Militar() {
+  let nome = "Cadastrar";
   return (
-    <Container>
+    <div id="container">
       <h1>Militar</h1>
-      <Box>
-        <Formulario>Formulario</Formulario>
-        <Registros>Registros</Registros>
-      </Box>
-    </Container>
+      <div id="box">
+        <div id="form">
+          <strong>Formulário</strong>
+          <input placeholder="Identidade Militar"></input>
+          <input placeholder="Nome"></input>
+          <input placeholder="Nome de Guerra"></input>
+          <input placeholder="Pelotão"></input>
+          <input placeholder="Senha" required type="password"></input>
+          <button>{nome}</button>
+          <button>Limpar</button>
+          <button>Cancelar</button>
+        </div>
+        <div id="registros">
+          <strong>Registros</strong>
+        </div>
+      </div>
+    </div>
   );
 }
 
