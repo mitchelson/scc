1ac685ef74be892f5fe5723a2e2ddb2c3927e900
Mitchelson <mitchelsonps@gmail.com>
2020-04-03 12:37:08 -0400

    Clean code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Login from './pages/Login';
 import PrivateRoute from './components/PrivateRoute';
-import GlobalStyle, { Container } from './styles/index';
+import GlobalStyle from './styles/index';
 
 import './global.css';
 import Routes from './routes';
